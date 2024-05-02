1274d56374121b34c2a99c9f1e1774efbcd309bf
lwrb172 <lwrbielak@gmail.com>
2024-05-02 10:47:45 +0200

    lab3z5b

--- src/SvgScene.java
+++ src/SvgScene.java
@@ -6,14 +6,20 @@ import java.util.Locale;
 
 public class SvgScene {
     private final List<Shape> shapes = new ArrayList<>();
-
     private static SvgScene instance = null;
+    private List<String> defs = new ArrayList<>();
+    private static int index = 0;
 
     public static SvgScene getInstance() {
         if(instance == null) {
             instance = new SvgScene();
         }
         return instance;
+    }
+
+    public int addFilter(String filter) {
+        defs.add(String.format(filter, ++index));
+        return index;
     }
 
     public void adShape(Shape shape) {
@@ -30,7 +36,11 @@ public class SvgScene {
                             "<svg height=\"%f\" width=\"%f\" xmlns=\"http://www.w3.org/2000/svg\">\n",
                             1000.0, 1000.0)
             );
-
+            fileWriter.write("<defs>");
+            for(String def : defs) {
+                fileWriter.write(def + "\n");
+            }
+            fileWriter.write("</defs");
             for (Shape shape : shapes) {
                 fileWriter.write("\t" + shape.toSvg("") + "\n");
             }
